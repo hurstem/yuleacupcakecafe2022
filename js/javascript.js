499f18d8d651b24793f5adee639baf701ca042aa
Emmie <99740597+hurstem@users.noreply.github.com>
2022-05-19 10:56:16 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,40 @@
+// init Masonry
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
+});
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
+
+$(document).ready(function(){
+
+
+
+	$("#navigation").sticky({
+		topSpacing : 75,
+	});
+
+	$('#nav').onePageNav({
+		currentClass: 'current',
+		changeHash: false,
+		scrollSpeed: 15000,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'easeInOutExpo'
+	});
+
+     $('#top-nav').onePageNav({
+         currentClass: 'active',
+         changeHash: true,
+         scrollSpeed: 1200
+    });
+//Initiat WOW JS
+    new WOW().init();
+
+});
+
+
